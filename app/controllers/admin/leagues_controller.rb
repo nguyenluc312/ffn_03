7efbb6aac6670475dc320ee5a1b756eb33cf3ee0
Do Gia Dat <dogiadat@gmail.com>
2016-07-11 14:41:43 +0700

    delete-league

--- app/controllers/admin/leagues_controller.rb
+++ app/controllers/admin/leagues_controller.rb
@@ -1,4 +1,9 @@
 class Admin::LeaguesController < ApplicationController
+  before_action :load_league, only: :destroy
+
+  def index
+    @leagues = League.order created_at: :desc
+  end
 
   def new
     @countries = Country.all.map {|country| [country.name, country.id]}
@@ -15,8 +20,29 @@ class Admin::LeaguesController < ApplicationController
     end
   end
 
+  def destroy
+    if @league.league_seasons.any?
+      flash[:danger] = t ".has_league_seasons"
+    else
+      if @league.destroy
+        flash[:success] = t ".success"
+      else
+        flash[:danger] = t ".failed"
+      end
+    end
+    redirect_to admin_leagues_url
+  end
+
   private
   def league_params
     params.require(:league).permit :name, :country_id
+  end
+
+  def load_league
+    @league = League.find_by id: params[:id]
+    unless @league
+      flash[:warning] = t ".warning"
+      redirect_to admin_leagues_url
+    end
   end
 end
