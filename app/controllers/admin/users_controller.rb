9c9c3fcc9815773870e4d10356742f3782e77f8a
hieuhtk58 <hieuht_58@vnu.edu.vn>
2016-07-12 17:20:48 +0700

    Admin delete user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,21 @@
 class Admin::UsersController < ApplicationController
+  before_action :load_user, only: [:destroy]
+
   def index
     @users = User.order("created_at DESC").page(params[:page])
       .per Settings.per_page
   end
+  def destroy
+    if @user.destroy
+      flash[:success] = t "views.admin.flash.success"
+    else
+      flash[:danger] = t "views.admin.flash.failed"
+    end
+    redirect_to admin_users_url
+  end
 
+  private
+  def load_user
+    @user = User.find_by id: params[:id]
+  end
 end
